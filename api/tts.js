d91eb833ced3dc4d7a206cc707d26cad301eef11
josiahsms <137184622+josiahsms@users.noreply.github.com>
2025-04-10 08:30:12 -0500

    added mygas v2

--- api/tts.js
+++ api/tts.js
@@ -21,7 +21,7 @@ export default async function handler(req, res) {
       },
       body: JSON.stringify({
         text,
-        model_id: 'eleven_monolingual_v1',
+        model_id: 'eleven_flash_v2',
         voice_settings: {
           stability: 0.5,
           similarity_boost: 0.75,
@@ -30,6 +30,11 @@ export default async function handler(req, res) {
               {
                 word: "Josiah",
                 phoneme: "ˈdʒoʊzaɪə",
+                alphabet: "ipa"
+              }
+              {
+                word: "MYGAs",
+                phoneme: "maɪɡʌz",
                 alphabet: "ipa"
               }
             ]
